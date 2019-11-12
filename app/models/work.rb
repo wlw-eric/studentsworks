457cfdbae6b74d6fcc20ef7a07c7644f0e9c7040
Pauline <pauline.eustachy@gmail.com>
2019-11-12 11:59:04 +0100

    model work relation changed

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  belongs_to :creator, class_name: :User, through: :project
+  has_one :creator, class_name: :User, through: :project
   belongs_to :speaker, class_name: :User
   belongs_to :project
 end
