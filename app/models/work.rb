06e4d6aa4dfd5e225d672c312c191f1a3982c315
Teix <marcoteixeira.57@gmail.com>
2019-11-12 14:13:02 +0100

    Work controller + validates work
    Work controller + validates work

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,5 @@ class Work < ApplicationRecord
   has_one :creator, class_name: :User, through: :project
   belongs_to :speaker, class_name: :User
   belongs_to :project
+  validates :project_id, uniqueness: true
 end
