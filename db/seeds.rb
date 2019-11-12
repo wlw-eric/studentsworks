4e6581ef5bf19e3c9779ac2fb0f416e2a485140b
Teix <marcoteixeira.57@gmail.com>
2019-11-12 12:36:55 +0100

    seeds
    seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-bob = User.create(
-  first_name: "b"
+10.times do
+  users = User.new(
+      email:    Faker::Internet.email,
+      password:    "123456",
+      first_name:    Faker::Name.first_name ,
+      last_name:    Faker::Name.last_name,
+      entreprise_name: Faker::Company.name,
+      role:  rand(1..2) #1-entreprise 2-intervenant
   )
+  users.save!
+end
+
+10.times do
+  project = Project.new(
+      name: Faker::Lorem.words(number: 3),
+      description:    Faker::Lorem.paragraph,
+      objective:    Faker::Lorem.sentence,
+      progress:    rand(1..3) , #1-nouveau 2-reserve 3-cloturé
+      picture:    "https://image.shutterstock.com/image-vector/rocket-development-rounded-icon-style-260nw-718385470.jpg",
+      category: ["entreprise", "speaker"].sample,
+      creator_id: rand(1..10)
+  )
+  project.save!
+end
