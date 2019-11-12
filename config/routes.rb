160f02459f85a16e240164a957755eccc1f61bba
Pauline <pauline.eustachy@gmail.com>
2019-11-12 15:39:53 +0100

    authentication needed to access the website

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   
   resources :works, only: [:index, :create]
 
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  root to: 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
