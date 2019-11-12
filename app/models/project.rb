0aa28d27ecac4346d5dd938f6b4bbe74d4bd3597
Pauline <pauline.eustachy@gmail.com>
2019-11-12 12:39:39 +0100

    models validation started

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ApplicationRecord
   # belongs_to :creator_id
   belongs_to :creator, class_name: :User
   has_one :work
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :objective, presence: true
+  validates :progress, presence: true, default: 1
+  validates :category, presence: true, inclusion: { in: ["Digital", "Marketing", "Design"] }
 end
