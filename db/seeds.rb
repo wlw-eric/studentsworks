77c4618f6b20e4cf80fb92898a18326f85deb016
jonathan chaffanjon <platonformation@gmail.com>
2019-11-12 14:53:14 +0100

    erreur mail de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 p "test"
 users = User.new(
-      email:    toto@gmail.com,
+      email:    "toto@gmail.com",
       password:    "123456",
       first_name:    Faker::Name.first_name ,
       last_name:    Faker::Name.last_name,
