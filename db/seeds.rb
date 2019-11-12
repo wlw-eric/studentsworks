073081f3da14fd2e7229fa4b00eedc4e4c0b5741
jonathan chaffanjon <platonformation@gmail.com>
2019-11-12 13:58:03 +0100

    pour PR

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "test"
 users = User.new(
       email:    toto@gmail.com,
       password:    "123456",
