d710a6c4a42f7c3ea64c29fd92955d56e772374a
Pauline Eustachy <pauline.eustachy@gmail.com>
2019-11-12 14:44:20 +0100

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -62,9 +62,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def user_projects
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
