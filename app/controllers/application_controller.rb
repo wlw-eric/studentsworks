325f78c347e2c3dbc1e2b06115834d23cd54e620
Teix <marcoteixeira.57@gmail.com>
2019-11-13 11:33:45 +0100

    email unique + sign up ok
    email unique + sign up ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,5 +17,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :entreprise_name, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :entreprise_name, :password])
   end
 end
