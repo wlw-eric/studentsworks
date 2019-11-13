3e8d88aa54b7f83e9b6600e8034ced6b27608181
Pauline <pauline.eustachy@gmail.com>
2019-11-13 13:01:10 +0100

    Destroy_all en premier dans seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-p "test"
+Project.destroy_all
+User.destroy_all
+
 users = User.new(
       email:    "toto@gmail.com",
       password:    "123456",
