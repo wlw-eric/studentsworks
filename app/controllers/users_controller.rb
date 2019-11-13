de77a0ebebf3d1299111a0c46f7114879b6bc4e0
Teix <marcoteixeira.57@gmail.com>
2019-11-13 16:11:21 +0100

    fix requete my projects
    fix requete my project pour ramener uniquement projet crée

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def my_projects
-    @my_projects = Project.joins("LEFT JOIN works ON works.project_id = projects.id")
+    @my_projects = Project.joins("LEFT JOIN works ON works.project_id = projects.id WHERE projects.creator_id = #{current_user.id} OR works.speaker_id = #{current_user.id}")
   end
 end
