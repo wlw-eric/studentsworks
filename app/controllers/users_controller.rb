06dd4bb0922b6cc78e41b6cd59f1798cb8bdf057
Teix <marcoteixeira.57@gmail.com>
2019-11-14 15:47:31 +0100

    Auto stash before rebase of "origin/master"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
-  def my_projects
-    @my_projects = Project.joins("LEFT JOIN works ON works.project_id = projects.id WHERE projects.creator_id = #{current_user.id} OR works.speaker_id = #{current_user.id}")
+  before_action :set_my_projects, :propal_projects, :closed_projects, :selected_projects
+
+  private
+
+  def set_my_projects
+    @my_projects = Project.joins("LEFT JOIN works ON
+      works.project_id = projects.id WHERE (projects.creator_id = #{current_user.id}
+      OR works.speaker_id = #{current_user.id})")
+  end
+
+  def propal_projects
+    @propal_projects = Project.where("projects.creator_id = #{current_user.id}")
+  end
+
+  def closed_projects
+    @closed_projects = Project.joins("LEFT JOIN works ON
+      works.project_id = projects.id WHERE projects.progress = 3
+      AND works.speaker_id = #{current_user.id}")
+  end
+
+  def selected_projects
+    @selected_projects = Project.joins("LEFT JOIN works ON
+      works.project_id = projects.id WHERE works.speaker_id = #{current_user.id}
+      AND projects.progress = 2")
   end
 end
