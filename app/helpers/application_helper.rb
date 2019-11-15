b77a489ad3303860ce64ff5064cdb3952f3e06cf
eric-wlw <weloveworks@gmail.com>
2019-11-15 16:43:37 +0100

    last change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
     if project.picture.present?
       cl_image_tag( @project.picture , width: 300, height: 300, crop: :fill)
     else
-      image_tag 'logo.png', alt: "Mon project", width: 300, height: 300
+      image_tag 'logo.png', alt: "My project", width: 300, height: 300
     end
   end
   def project_imageindex_for(project)
     if project.picture.present?
       cl_image_tag( project.picture , width: 250, height: 200, crop: :fill)
     else
-      image_tag 'defaut-project.jpg', alt: "Mon project", width: 250, height: 200
+      image_tag 'defaut-project.jpg', alt: "My project", width: 250, height: 200
     end
   end
 end
